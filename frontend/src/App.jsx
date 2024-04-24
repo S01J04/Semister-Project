25cad33dc6a5ff9bb30c5ddf59bd2e43ad34a4e8
SO1JO4 <muhammad12345sohaib@gmail.com>
2024-04-24 10:13:16 -0700

    COmmiit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,11 @@
-import Header from "./components/Header";
-import Body_search from "./components/Body_search"
-import Discoverbillboards from "./components/Discoverbillborads"
-import Vediopage from "./components/vediopage"
-import Contactus from "./components/Contactus";
-import Typeofbillboards from "./components/typeofbillboards";
-import Billboardrequest from "./components/Billboardrequest";
-import Province from "./components/Province";
-import { FooterFour } from "./components/Footer";
-import Ourclients from "./components/Ourclients";
-import UserReview from "./components/UserReview";
+import HomePage from "./HomePage"
+
 function App() {
 
 
   return (
     < >
-     <Header  />
-     <Body_search/>
-     <Discoverbillboards/>
-      <Vediopage/>
-      <Contactus/>
-      <Typeofbillboards/>
-      <Billboardrequest/>
-      <Province/>
-      <UserReview/>
-      <Ourclients/>
-      <FooterFour/>
+     <HomePage/>      
     </>
   )
 }
---
+++ frontend/src/HomePage.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Header from "./components/Header";
+import Body_search from "./components/Body_search"
+import Discoverbillboards from "./components/Discoverbillborads"
+import Vediopage from "./components/vediopage"
+import Contactus from "./components/Contactus";
+import Typeofbillboards from "./components/typeofbillboards";
+import Billboardrequest from "./components/Billboardrequest";
+import Province from "./components/Province";
+import { FooterFour } from "./components/Footer";
+import Ourclients from "./components/Ourclients";
+import UserReview from "./components/UserReview";
+const HomePage = () => {
+  return (
+    <>
+      <Header  />
+      <Body_search/>
+      <Discoverbillboards/>
+      <Vediopage/>
+      <Contactus/>
+      <Typeofbillboards/>
+      <Billboardrequest/>
+      <Province/>
+      <UserReview/>
+      <Ourclients/>
+      <FooterFour/>
+    </>
+  )
+}
+
+export default HomePage
